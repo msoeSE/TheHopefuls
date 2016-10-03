7d05fe28c7582b2b2c9864ed2370e96e0dc61862
Patrick Zawadzki <zawadzkip@msoe.edu>
2016-10-03 17:44:50 -0500

    initial Xamarin Forms project

---
+++ StudentDriver/StudentDriver/StudentDriverPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace StudentDriver
+{
+	public partial class StudentDriverPage : ContentPage
+	{
+		public StudentDriverPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
