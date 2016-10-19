d2297dbe46e15b38447a4d868824a900522ad039
Dylan Walseth <dylanwalseth@gmail.com>
2016-10-19 16:49:31 -0500

    Re-did the core, started from scratch and used a handy tutorial, im sure more work will need to be done

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,15 +1,35 @@
-// Require babel hook included to load all subsequent files required by
-// gulp with the extensions .es6, .es, .jsx, .js and transpile them
-// with babel. This will also automatically require the polyfill.
-require("babel-register");
-// ```
-// gulpfile.js
-// (c) 2016 David Newman
-// blackshuriken@hotmail.com
-// gulpfile.js may be freely distributed under the MIT license
-// ```
+/*eslint-disable */
+var gulp = require("gulp");
+var eslint = require("gulp-eslint");
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var pump = require('pump');
 
-// *gulpfile.js*
+gulp.task("lint", function() {
+    return gulp.src(["src/**/*.js", "!src/**/sample/**/*.js", "!src/tests/**/*.js", "!src/**/*.min.*", "!src/public/**/*.js"])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
 
-// Load gulp configuration
-var conf = require('./gulpfile.conf.js');
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src('public/js/**/*.js'),
+        uglify(),
+        gulp.dest('dist/js/')
+    ],
+    cb
+  );
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('public/css/**/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css/'));
+});
