4c6b6cd1bb160f71ec9a5e67c62f7275ff9b3697
Patrick Zawadzki <zawadzkip@msoe.edu>
2016-11-04 14:23:23 -0500

    Adding basic sqlite db code

---
+++ StudentDriver/Droid/SQLite_Android.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using SQLite;
+
+namespace StudentDriver.Droid
+{
+	public class SQLite_Android : ISQLite
+	{
+		private string dbName = "StudentDriver.db3";
+
+		public SQLiteAsyncConnection GetAsyncConnection ()
+		{
+			string docPath = System.Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+			var path = Path.Combine (docPath, dbName);
+			var conn = new SQLite.SQLiteAsyncConnection (path);
+			return conn;
+		}
+
+		[Obsolete ("Use GetAsyncConnection instead")]
+		public SQLiteConnection GetConnection ()
+		{
+
+			string docPath = System.Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+			var path = Path.Combine (docPath, dbName);
+			var conn = new SQLite.SQLiteConnection (path);
+			return conn;
+		}
+	}
+}
---
+++ StudentDriver/StudentDriver/Services/SQLiteDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SQLite;
+using Xamarin.Forms;
+
+namespace StudentDriver
+{
+	public class SQLiteDatabase
+	{
+		static SQLiteAsyncConnection _database;
+		public SQLiteDatabase ()
+		{
+			_database = DependencyService.Get<ISQLite> ().GetAsyncConnection ();
+			//TODO Create tables here
+		}
+
+	}
+}
