21cf9f09eb02c53a0e86a755a1b6466910f22db3
Patrick Zawadzki <zawadzkip@msoe.edu>
2016-11-06 21:21:39 -0600

    adding some Stats page UI

---
+++ StudentDriver/StudentDriver/CustomControls/TitleLabel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+namespace StudentDriver
+{
+	public class TitleLabel : Label
+	{
+		public TitleLabel ()
+		{
+			this.FontAttributes = FontAttributes.Bold;
+			this.FontSize = Device.GetNamedSize (NamedSize.Large, typeof (Label));
+		}
+	}
+}
