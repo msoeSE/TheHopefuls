88b6ac41c5c987b705b816b5ccf48ebf8f7bd570
Patrick Zawadzki <zawadzkip@msoe.edu>
2016-11-08 09:32:23 -0600

    adding some UI animations

--- StudentDriver/StudentDriver/StatsPage.xaml.cs
+++ StudentDriver/StudentDriver/StatsPage.xaml.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using System.Threading.Tasks;
 
 namespace StudentDriver
 {
 	public partial class StatsPage : ContentPage
 	{
+		private string daytimeDistanceText = "";
+		private string nighttimeDistnaceText = "";
+		private string totalDistanceText = "";
+		private string totalTimeText = "";
 
+		protected override async void OnAppearing ()
+		{
+			base.OnAppearing ();
+			await Task.Run (() => {
+				totalDistanceProgress.ProgressTo (0.6, 1500, Easing.Linear);
+				daytimeDistanceProgress.ProgressTo (0.333, 1500, Easing.Linear);
+				nighttimeDistanceProgress.ProgressTo (1.0, 1500, Easing.Linear);
+			});
+
+
+		}
 		public StatsPage ()
 		{
 			InitializeComponent ();
+			daytimeDistanceText = daytimeDistanceLabel.Text;
+			nighttimeDistnaceText = nighttimeDistanceLabel.Text;
+			totalDistanceText = totalDistanceLabel.Text;
+			totalTimeText = totalTimeLabel.Text;
 			daytimeDistanceLabel.GestureRecognizers.Add (new TapGestureRecognizer (daytimeDistanceLabelPressed));
 			nighttimeDistanceLabel.GestureRecognizers.Add (new TapGestureRecognizer (nighttimeDistanceLabelPressed));
 			totalDistanceLabel.GestureRecognizers.Add (new TapGestureRecognizer (totalDistanceLabelPressed));
@@ -19,23 +39,40 @@ namespace StudentDriver
 
 		void daytimeDistanceLabelPressed (View pressedLabel, object arg2)
 		{
-			daytimeDistanceLabel.Text = "33%";
+			if (daytimeDistanceLabel.Text.Equals (daytimeDistanceText)) {
+				daytimeDistanceLabel.Text = "33%";
+			} else {
+				daytimeDistanceLabel.Text = daytimeDistanceText;
+			}
+
 		}
 
 		void nighttimeDistanceLabelPressed (View pressedLabel, object arg2)
 		{
-			nighttimeDistanceLabel.Text = "100%";
+			if (nighttimeDistanceLabel.Text.Equals (nighttimeDistnaceText))
+				nighttimeDistanceLabel.Text = "100%";
+			else {
+				nighttimeDistanceLabel.Text = nighttimeDistnaceText;
+			}
 		}
 
 		void totalDistanceLabelPressed (View pressedLabel, object arg2)
 		{
 			Label label = (Label)pressedLabel;
-			label.Text = "60%";
+			if (label.Text.Equals (totalDistanceText))
+				label.Text = "60%";
+			else {
+				label.Text = totalDistanceText;
+			}
 		}
 
 		void timeLabelPressed (View arg1, object arg2)
 		{
-			totalTimeLabel.Text = "Flut shots are fake.";
+			if (totalTimeLabel.Text.Equals (totalTimeText))
+				totalTimeLabel.Text = "Flut shots are fake.";
+			else {
+				totalTimeLabel.Text = totalTimeText;
+			}
 		}
 	}
 }
