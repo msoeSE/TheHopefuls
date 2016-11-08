3c548dd6278de82f9bd79730367c872cd8ea3f5a
Patrick Zawadzki <zawadzkip@msoe.edu>
2016-11-08 12:33:39 -0600

    adding renderer for progressbar, added states to stats page

---
+++ StudentDriver/iOS/Renderers/CustomProgressBarRenderer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using StudentDriver.iOS;
+using CoreGraphics;
+using UIKit;
+[assembly: ExportRenderer (typeof (ProgressBar), typeof (CustomProgressBarRenderer))]
+namespace StudentDriver.iOS
+{
+	public class CustomProgressBarRenderer : ProgressBarRenderer
+	{
+		protected override void OnElementChanged (ElementChangedEventArgs<ProgressBar> e)
+		{
+			base.OnElementChanged (e);
+			if (Control != null) {
+				Control.ProgressTintColor = UIColor.Orange;
+			}
+		}
+	}
+}
