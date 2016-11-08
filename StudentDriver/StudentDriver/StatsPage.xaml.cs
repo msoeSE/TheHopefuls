7e5023fe8f879c8b76aed45825f585f230d8c020
Patrick Zawadzki <zawadzkip@msoe.edu>
2016-11-07 18:02:53 -0600

    basic ui for statistics

--- StudentDriver/StudentDriver/StatsPage.xaml.cs
+++ StudentDriver/StudentDriver/StatsPage.xaml.cs
@@ -7,9 +7,35 @@ namespace StudentDriver
 {
 	public partial class StatsPage : ContentPage
 	{
+
 		public StatsPage ()
 		{
 			InitializeComponent ();
+			daytimeDistanceLabel.GestureRecognizers.Add (new TapGestureRecognizer (daytimeDistanceLabelPressed));
+			nighttimeDistanceLabel.GestureRecognizers.Add (new TapGestureRecognizer (nighttimeDistanceLabelPressed));
+			totalDistanceLabel.GestureRecognizers.Add (new TapGestureRecognizer (totalDistanceLabelPressed));
+			totalTimeLabel.GestureRecognizers.Add (new TapGestureRecognizer (timeLabelPressed));
+		}
+
+		void daytimeDistanceLabelPressed (View pressedLabel, object arg2)
+		{
+			daytimeDistanceLabel.Text = "33%";
+		}
+
+		void nighttimeDistanceLabelPressed (View pressedLabel, object arg2)
+		{
+			nighttimeDistanceLabel.Text = "100%";
+		}
+
+		void totalDistanceLabelPressed (View pressedLabel, object arg2)
+		{
+			Label label = (Label)pressedLabel;
+			label.Text = "60%";
+		}
+
+		void timeLabelPressed (View arg1, object arg2)
+		{
+			totalTimeLabel.Text = "Flut shots are fake.";
 		}
 	}
 }
