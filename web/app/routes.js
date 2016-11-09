ffe66893daf3634d317cdef19eb973becd2bf4f4
Gerald <sorianog@msoe.edu>
2016-11-08 20:50:25 -0600

    #20 started defining api endpoints

--- web/app/routes.js
+++ web/app/routes.js
@@ -4,6 +4,37 @@ module.exports = function(app) {
 	// handle things like api calls
 	// authentication routes
 
+	// TODO: add handling for bad api calls (i.e. invalid params, bad data formats)
+  app.get("/api/students/:userId", function(req, res) {
+		res.statusCode = 200;
+		res.json({ message: "Successful request!" });
+	});
+
+	app.get("/api/students/:userId/drivingsessions", function(req, res) {
+    res.statusCode = 200;
+    res.json({});
+	});
+
+	app.get("/api/drivingschools", function(req, res) {
+		res.statusCode = 200;
+		res.json({});
+	});
+
+	app.get("/api/drivingschools/:schoolId", function(req, res) {
+		res.statusCode = 200;
+		res.json({});
+	});
+
+	app.get("/api/drivingschools/:schoolId/students", function(req, res) {
+		res.statusCode = 200;
+		res.json({});
+	});
+
+	app.get("/api/drivingschools/:schoolId/instructors", function(req, res) {
+		res.statusCode = 200;
+		res.json({});
+	});
+
 	// frontend routes =========================================================
 	// route to handle all angular requests
 	app.get('*', function(req, res) {
