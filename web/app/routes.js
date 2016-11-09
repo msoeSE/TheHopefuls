8581301892a782b08ac4efbfbfe12b844c32402b
Gerald <sorianog@msoe.edu>
2016-11-09 17:44:13 -0600

    #20 defined more api endpoints

--- web/app/routes.js
+++ web/app/routes.js
@@ -5,18 +5,37 @@ module.exports = function(app) {
 	// authentication routes
 
 	// TODO: add handling for bad api calls (i.e. invalid params, bad data formats)
-  app.get("/api/students/:userId", function(req, res) {
+
+	app.get("/api/students/:userId", function(req, res) {
 		res.statusCode = 200;
 		res.json({ message: "Successful request!" });
 	});
 
+	// allow a user to be added, return the id for the user
+	app.post("/api/students/", function(req, res) {
+		res.statusCode = 201;
+		res.json({});
+	});
+
 	app.get("/api/students/:userId/drivingsessions", function(req, res) {
-    res.statusCode = 200;
-    res.json({});
+		res.statusCode = 200;
+		res.json({});
+	});
+
+	// allow a new driving session to be added, return the data added
+	app.post("/api/students/:userId/drivingsessions", function(req, res) {
+		res.statusCode = 201;
+		res.json({});
 	});
 
 	app.get("/api/drivingschools", function(req, res) {
 		res.statusCode = 200;
+		res.json({});
+	});
+
+	// allow a new driving school to be added, return the data added
+	app.post("/api/drivingschools", function(req, res) {
+		res.statusCode = 201;
 		res.json({});
 	});
 
@@ -30,11 +49,26 @@ module.exports = function(app) {
 		res.json({});
 	});
 
+	app.delete("/api/drivingschools/:schoolId/students/:userId", function(req, res) {
+		//res.statusCode = 200;
+		res.json({});
+	});
+
 	app.get("/api/drivingschools/:schoolId/instructors", function(req, res) {
 		res.statusCode = 200;
 		res.json({});
 	});
 
+	// allow a new instructor to be added, return the data added
+	app.post("/api/drivingschools/:schoolId/instructors", function(req, res) {
+		res.statusCode = 201;
+		res.json({});
+	});
+
+	app.delete("/api/drivingschools/:schoolId/instructors/:userId", function(req, res) {
+		//res.statusCode = 201;
+		res.json({});
+	});
 	// frontend routes =========================================================
 	// route to handle all angular requests
 	app.get('*', function(req, res) {
