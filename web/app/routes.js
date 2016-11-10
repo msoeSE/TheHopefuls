7fb108fe670c5e977dde1b783d48030c9145d3c2
Gerald <sorianog@msoe.edu>
2016-11-09 19:48:05 -0600

    #20 more work on endpoints

--- web/app/routes.js
+++ web/app/routes.js
@@ -1,3 +1,8 @@
+var mongoose = require("mongoose");
+
+var User = require("./models/User.js");
+var DrivingSchool = require("./models/DrivingSchool.js");
+
 module.exports = function(app) {
 
 	// server routes ===========================================================
@@ -7,14 +12,33 @@ module.exports = function(app) {
 	// TODO: add handling for bad api calls (i.e. invalid params, bad data formats)
 
 	app.get("/api/students/:userId", function(req, res) {
-		res.statusCode = 200;
-		res.json({ message: "Successful request!" });
+		User.find({ userId: req.params.userId }, function(err, doc) {
+			if(!err) {
+				res.statusCode = 200;
+				res.json(doc);
+			} else {
+				res.send(err);
+			}
+		});
 	});
 
 	// allow a user to be added, return the id for the user
 	app.post("/api/students/", function(req, res) {
+		var student = {
+			userId: req.body.Id,
+			email: req.body.email,
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+			password: req.body.password,
+			userType: req.body.userType
+			// state: req.body.state
+		};
+
+		var data = new User(student);
+		data.save();
+
 		res.statusCode = 201;
-		res.json({});
+		res.json(data);
 	});
 
 	app.get("/api/students/:userId/drivingsessions", function(req, res) {
