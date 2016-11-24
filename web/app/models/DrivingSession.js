34b4237462d69c21cd13b2bac1c4fbdb1a319dba
Gerald <sorianog@msoe.edu>
2016-11-23 18:12:14 -0600

    #16 Made changes to models

--- web/app/models/DrivingSession.js
+++ web/app/models/DrivingSession.js
@@ -4,8 +4,8 @@ var mongoose = require("mongoose");
 // define our driving session model
 // module.exports allows us to pass this to other files when it is called
 var DrivingSessionSchema = new mongoose.Schema({
-    startTime: { type: Number, required: true },
-    endTime: { type: Number, required: true },
+    startTime: { type: String, required: true },
+    endTime: { type: String, required: true },
     distance: { type: Number, required: true },
     duration: { type: Number, required: true },
     weatherData: [String]
--- web/app/models/User.js
+++ web/app/models/User.js
@@ -8,7 +8,6 @@ var UserSchema = new mongoose.Schema({
     email: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    password: { type: String, required: true },
     userType: { type: String, required: true },
     state: String,
     drivingSessions: [{ type: Schema.Types.ObjectId, ref: "DrivingSession" }]
