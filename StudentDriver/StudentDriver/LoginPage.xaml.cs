2dd344fbf70903627982eba4cc09b86cc73b5f35
Patrick Zawadzki <zawadzkip@msoe.edu>
2016-11-30 14:52:37 -0600

    bare login screen

---
+++ StudentDriver/StudentDriver/LoginPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StudentDriver
+{
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+			facebookLogin.GestureRecognizers.Add (new TapGestureRecognizer (async (View arg1, object arg2) => {
+				await FacebookFrameTapped (arg1, arg2);
+			}));
+			googleLogin.GestureRecognizers.Add (new TapGestureRecognizer (async (View arg1, object arg2) => {
+				await GoogleLoginTapped (arg1, arg2);
+			}));
+		}
+
+		async Task GoogleLoginTapped (View arg1, object arg2)
+		{
+			googleLogin.Opacity = 0.25;
+			await googleLogin.FadeTo (1.0);
+			App.Current.MainPage = new StudentDriverPage ();
+		}
+
+		async Task FacebookFrameTapped (View arg1, object arg2)
+		{
+			facebookLogin.Opacity = 0.25;
+			await facebookLogin.FadeTo (1.0);
+			App.Current.MainPage = new StudentDriverPage ();
+		}
+	}
+}
