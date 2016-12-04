ab90fc350ae5ac3ce6238729a9d5d48d3e33ba7a
Gerald <sorianog@msoe.edu>
2016-12-04 17:06:46 -0600

    Added Schema as a var for models

--- web/app/models/DrivingSession.js
+++ web/app/models/DrivingSession.js
@@ -1,9 +1,10 @@
 // grab the mongoose module
 var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
 // define our driving session model
 // module.exports allows us to pass this to other files when it is called
-var DrivingSessionSchema = new mongoose.Schema({
+var DrivingSessionSchema = new Schema({
     startTime: { type: Date, required: true },
     endTime: { type: Date, required: true },
     distance: { type: Number, required: true },
