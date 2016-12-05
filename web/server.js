76698219fff9486efcf7518f93d3891ab74f3ee5
Dylan Walseth <dylanwalseth@gmail.com>
2016-12-05 12:45:48 -0600

    travis file moved to root, some work on login

--- web/server.js
+++ web/server.js
@@ -4,30 +4,107 @@ var app            = express();
 var mongoose       = require("mongoose");
 var bodyParser     = require("body-parser");
 var methodOverride = require("method-override");
+var session = require("express-session");
+var passport = require("passport");
+var passportGoogle = require("passport-google");
+var GoogleStrategy = passportGoogle.Strategy;
+var passportFacebook = require("passport-facebook");
+var FacebookStrategy = passportFacebook.Strategy;
+
 
 // configuration ===========================================
 
 // config files
 var db = require("./config/db");
 var prodPort = 80;
-var devPort = 8080;
+var devPort = 3000;
+var port = (process.env.NODE_ENV === "production" ? prodPort : devPort);
+
+// If you don't have this file, contact Dylan for it.
+var config = require("./config.json")
+
 // mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
-// get all data/stuff of the body (POST) parameters
-app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(methodOverride("X-HTTP-Method-Override"));
+app.use(session({
+	secret: config.Auth.Secret,
+	name: "sessId",
+	resave: true,
+	saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+	done(null, obj);
+});
+
 if(process.env.NODE_ENV === "production") {
 	app.use(express.static(__dirname + "/dist"));
-	var port = process.env.PORT || prodPort;
+
 } else {
 	app.use(express.static(__dirname + "/public"));
-	var port = process.env.PORT || devPort;
+
+	passport.use(new GoogleStrategy({
+		returnURL: `http://localhost:${port}/auth/google/return`,
+		realm: `http://localhost:${port}/`
+	},
+	function(identifier, done) {
+		User.findByOpenID({ openId: identifier }, function (err, user) {
+			return done(err, user);
+		});
+	}));
+
+	passport.use(new FacebookStrategy({
+		clientID: config.Auth.FacebookAuth.Local.ID,
+		clientSecret: config.Auth.FacebookAuth.Local.Secret,
+		callbackURL: `http://localhost:${port}/auth/facebook/callback`,
+		profileFields: ["id", "email", "gender", "name"]
+	},
+	function(accessToken, refreshToken, profile, cb) {
+		// User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+		// 	return cb(err, user);
+		// });
+		return cb(null, profile);
+	}));
 }
 app.use("/libs", express.static(__dirname + "/libs"));
 
+app.get("/auth/facebook", passport.authenticate("facebook"));
+app.get("/auth/facebook/callback",
+	passport.authenticate("facebook", { failureRedirect: "/login" }),
+	function(req, res) {
+		res.redirect("/");
+	}
+);
+app.get("/auth/google", passport.authenticate("google"));
+app.get("/auth/google/return",
+	passport.authenticate("google", { failureRedirect: "/login" }),
+	function(req, res) {
+		res.redirect("/");
+	}
+);
+app.get("/profile",
+	function(req, res){
+		if(req.user)
+			res.write(JSON.stringify(req.user));
+		res.end();
+	}
+);
+app.get("/test", function(req, res){
+	res.write("Test");
+	res.end();
+});
+app.get("/auth/logout",
+	function(req, res){
+		req.logout();
+		res.redirect("/");
+	}
+);
 // routes ==================================================
 require("./app/routes")(app); // pass our application into our routes
 
