2f8d0f4b5ea62ffc36e501d1374de717ee0e188f
Patrick Zawadzki <zawadzkip@msoe.edu>
2016-12-12 12:45:34 -0600

    adding unit testing project

--- StudentDriver/StudentDriver/Services/SQLiteDatabase.cs
+++ StudentDriver/StudentDriver/Services/SQLiteDatabase.cs
@@ -8,21 +8,21 @@ namespace StudentDriver
 	public class SQLiteDatabase
 	{
 		static SQLiteAsyncConnection _database;
-	    private SQLiteDatabase _sqLiteDatabaseInstance;
+		private SQLiteDatabase _sqLiteDatabaseInstance;
 
-	    public SQLiteDatabase GetInstance()
-	    {
-	        return _sqLiteDatabaseInstance ?? (_sqLiteDatabaseInstance = new SQLiteDatabase());
-	    }
+		public SQLiteDatabase GetInstance ()
+		{
+			return _sqLiteDatabaseInstance ?? (_sqLiteDatabaseInstance = new SQLiteDatabase ());
+		}
 
-	    private SQLiteDatabase ()
+		private SQLiteDatabase ()
 		{
 			_database = DependencyService.Get<ISQLite> ().GetAsyncConnection ();
-		    _database.CreateTableAsync<StateReqs>();
-		    _database.CreateTableAsync<User>();
-		    _database.CreateTableAsync<UserStats>();
-	        _database.CreateTableAsync<DrivePoints>();
-;		    _database.CreateTableAsync<UnsyncDrives>();
+			_database.CreateTableAsync<StateReqs> ();
+			_database.CreateTableAsync<User> ();
+			_database.CreateTableAsync<UserStats> ();
+			_database.CreateTableAsync<DrivePoints> ();
+			_database.CreateTableAsync<UnsyncDrives> ();
 		}
 
 	}
