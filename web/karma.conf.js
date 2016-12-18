b19ca3e77d974f30354f5ef464ff92ccaffc496a
Nate <dobbinsn@msoe.edu>
2016-12-18 16:09:56 -0600

    testing works, so tests can be written now. Gulp tasks were also created. Read me was updated to reflect added testing functionality.

--- web/karma.conf.js
+++ web/karma.conf.js
@@ -10,12 +10,12 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'requirejs'],
+    frameworks: ['jasmine'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      {pattern: 'spec/**/*spec.js', included: false}
+      {pattern: 'spec/**/*spec.js', included: true}
     ],
 
 
@@ -33,7 +33,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['spec'],
 
 
     // web server port
