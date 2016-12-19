9cd3e332bdadf8ac50fe3bec2653133f6959f091
Patrick Zawadzki <zawadzkip@msoe.edu>
2016-12-19 12:33:44 -0600

    add blank oauth config
    add bare

---
+++ StudentDriver/StudentDriver/Config/OAuth.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace StudentDriver
+{
+	public class OAuth
+	{
+		public static string FACEBOOK_APP_ID { get; } = "";
+		public static string GOOGLE_APP_ID { get; } = "";
+		public OAuth ()
+		{
+		}
+	}
+}
