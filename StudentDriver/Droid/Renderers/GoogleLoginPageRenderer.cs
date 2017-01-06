d8237e11a177aaef83ee8b697b98429d47603453
Patrick Zawadzki <zawadzkip@msoe.edu>
2017-01-06 08:29:24 -0600

    adding settings plugin and some page renderers

---
+++ StudentDriver/Droid/Renderers/GoogleLoginPageRenderer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Xamarin.Forms.Platform.Android;
+using StudentDriver;
+using StudentDriver.Droid;
+using Xamarin.Forms;
+using Xamarin.Auth;
+using Android.App;
+using System.Net.Http;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+[assembly: ExportRenderer (typeof (GoogleLoginPage), typeof (GoogleLoginPageRenderer))]
+namespace StudentDriver.Droid
+{
+	public class GoogleLoginPageRenderer : PageRenderer
+	{
+		public GoogleLoginPageRenderer ()
+		{
+		}
+
+		protected override void OnElementChanged (ElementChangedEventArgs<Page> e)
+		{
+			base.OnElementChanged (e);
+			var activity = this.Context as Activity;
+			var auth = new OAuth2Authenticator (
+				clientId: OAuth.FACEBOOK_APP_ID,
+				scope: "email",
+				authorizeUrl: new Uri (OAuth.FACEBOOK_OAUTH_URL),
+				redirectUrl: new Uri (OAuth.FACEBOOK_SUCCESS));
+			auth.AllowCancel = false;
+			auth.Completed += async (sender, ev) => {
+				if (!ev.IsAuthenticated) {
+					return;
+				} else {
+					var access = ev.Account.Properties ["access_token"];
+					using (var client = new HttpClient ()) {
+						var content = new FormUrlEncodedContent (new [] {
+							new KeyValuePair<string,string>("accessToken",access),
+						});
+						var authResponse = await client.PostAsync (new Uri ("https://host.dylanwalseth.me/auth/facebook/token"), content);
+						if (authResponse.IsSuccessStatusCode) {
+							var responseContent = await authResponse.Content.ReadAsStringAsync ();
+							var authTicket = JsonConvert.DeserializeObject<AuthenticatedUser> (responseContent);
+							//TODO Find out what dylan is sending
+							if (authTicket != null) {
+								var apiAccessToken = authTicket.Access_Token;
+							}
+						}
+					}
+				}
+			};
+			activity.StartActivity (auth.GetUI (activity));
+		}
+	}
+
+	public class AuthenticatedUser
+	{
+		public string Access_Token {
+			get;
+			set;
+		}
+	}
+}
