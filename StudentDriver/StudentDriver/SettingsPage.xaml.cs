a58a546a0d4fd1156fb920390223954f58242cad
Patrick Zawadzki <zawadzkip@msoe.edu>
2017-01-12 21:28:04 -0600

    removing night mode

--- StudentDriver/StudentDriver/SettingsPage.xaml.cs
+++ StudentDriver/StudentDriver/SettingsPage.xaml.cs
@@ -12,21 +12,11 @@ namespace StudentDriver
 			InitializeComponent ();
 			//Just placeholder states to resize the elements
 			statePicker.Items.Add ("Wisconsin");
-			nightModePicker.Items.Add ("Off");
-			nightModePicker.Items.Add ("On");
-			nightModePicker.Items.Add ("Auto");
-			nightModePicker.SelectedIndex = 0;
-			nightModePicker.SelectedIndexChanged += NightModeSelected;
 			statePicker.SelectedIndexChanged += StateSelected;
 			logOutButton.Clicked += LogOutTapped;
 		}
 
 		void StateSelected (object sender, EventArgs e)
-		{
-
-		}
-
-		void NightModeSelected (object sender, EventArgs e)
 		{
 
 		}
