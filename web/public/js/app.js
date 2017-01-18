06fea095995a925f3e0e197c134297aa5b94fe86
Nate <dobbinsn@msoe.edu>
2017-01-18 16:36:14 -0600

    added initial files

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -1,1 +1,1 @@
-angular.module("mainApp", ["ngRoute", "appRoutes", "MainCtrl", "StatsCtrl", "StatsService"]);
+angular.module("mainApp", ["ngRoute", "appRoutes", "MainCtrl", "StatsCtrl", "SettingsCtrl", "StatsService"]);
