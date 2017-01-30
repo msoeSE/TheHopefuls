e0cdfbf51c4934e775a43353a2a8e577d7eb5e86
Nate Dobbins <dobbinsn@msoe.edu>
2017-01-30 12:55:13 -0600

    cleaned up the material_select call that wasnt working

--- web/public/js/controllers/SettingsCtrl.js
+++ web/public/js/controllers/SettingsCtrl.js
@@ -1,12 +1,7 @@
 angular.module("SettingsCtrl", ["SettingsService"]).controller("SettingsController", function($log, SettingsOb) {
 	var vm = this;
 
-	angular.element(document).ready(function() {
-		$("select").material_select();// need to find out why materialize logos and this dont work
-	});
-
 	SettingsOb.getProfile().then(function(settings){
-		$log.log(settings);
 		vm.username = settings.first_name + " " + settings.last_name;
 	});
 
