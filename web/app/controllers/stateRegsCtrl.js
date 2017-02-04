8ed1eb7725ec59e8321e89153205698868c1df63
Gerald Soriano <sorianog@msoe.edu>
2017-02-04 10:01:46 -0600

    #46 Finished test and formatted code. Thanks for the help Dy-lon.

--- web/app/controllers/stateRegsCtrl.js
+++ web/app/controllers/stateRegsCtrl.js
@@ -1,16 +1,16 @@
 var StateRegulations = require("../models/StateRegulations");
 
-exports.getStateRegs = function (id, callback, error) {
-  StateRegulations.findOne({
-    state: state
-  }, function(err, doc) {
-    if (err) {
-      error({
-        "message": "Error retrieving state regulations",
-        "error": err
-      });
-      return;
-    }
-    callback(doc);
-  });
+exports.getStateRegs = function (state, callback, error) {
+	StateRegulations.findOne({
+		state: state
+	}, function(err, doc) {
+		if (err) {
+			error({
+				"message": "Error retrieving state regulations",
+				"error": err
+			});
+			return;
+		}
+		callback(doc);
+	});
 };
