dafaec788ff96bb8d43613c6af9d1c4e205267ac
Patrick Zawadzki <zawadzkip@msoe.edu>
2017-03-22 10:53:32 -0500

    adding android images

--- StudentDriver/Droid/MainActivity.cs
+++ StudentDriver/Droid/MainActivity.cs
@@ -11,20 +11,20 @@ using Acr.UserDialogs;
 
 namespace StudentDriver.Droid
 {
-	[Activity (Label = "StudentDriver.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "Driving Log", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
-		protected override void OnCreate (Bundle bundle)
+		protected override void OnCreate(Bundle bundle)
 		{
 			TabLayoutResource = Resource.Layout.Tabbar;
 			ToolbarResource = Resource.Layout.Toolbar;
 
-			base.OnCreate (bundle);
+			base.OnCreate(bundle);
 
-			global::Xamarin.Forms.Forms.Init (this, bundle);
-			UserDialogs.Init (this);
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+			UserDialogs.Init(this);
 
-			LoadApplication (new App ());
+			LoadApplication(new App());
 		}
 	}
 }
