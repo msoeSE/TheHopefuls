d82bf25c6986d79c74cf44b28f46b3a56be302ce
Gerald Soriano <sorianog@msoe.edu>
2017-03-21 21:44:36 -0500

    #71 minor formatting fix

--- web/app/controllers/linkSchoolCtrl.js
+++ web/app/controllers/linkSchoolCtrl.js
@@ -4,7 +4,7 @@ var drivingSchoolCtrl = require("../controllers/drivingSchoolCtrl");
 
 // update user with schoolId for linking
 exports.linkAccToSchool = function (userId, schoolId, callback, error) {
-	User.findOneAndUpdate({ userId: userId}, {schoolId: schoolId}, function(err, student) {
+	User.findOneAndUpdate({userId: userId}, {schoolId: schoolId}, function(err, student) {
 		if (err) {
 			error({
 				"message": "Error finding student to link school with",
