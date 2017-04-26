24d0de061a0605f200ded832c272e461506717eb
Nate Dobbins <dobbinsn@msoe.edu>
2017-04-25 22:49:19 -0500

    fixed bug in linkacctoschool endpoint

--- web/app/controllers/linkSchoolCtrl.js
+++ web/app/controllers/linkSchoolCtrl.js
@@ -4,19 +4,33 @@ var drivingSchoolCtrl = require("../controllers/drivingSchoolCtrl");
 
 // update user with schoolId for linking
 exports.linkAccToSchool = function(userId, schoolId, callback, error) {
-	User.findOneAndUpdate({
-		userId: userId
-	}, {
-		schoolId: schoolId
-	}, function(err, student) {
-		if (err) {
-			error({
-				"message": "Error finding student to link school with",
-				"error": err
+	drivingSchoolCtrl.getSchool(schoolId,
+	(results) => {
+		if(results != null){
+			User.findOneAndUpdate({
+				userId: userId
+			}, {
+				schoolId: schoolId
+			}, function(err, student) {
+				if (err) {
+					error({
+						"message": "Error finding student to link school with",
+						"error": err
+					});
+					return;
+				}
+				drivingSchoolCtrl.addStudentToSchool(schoolId, student,
+					(school) => {
+						callback({student: student, school: school});
+					}, (schoolError) => {
+						callback(schoolError);
+					});
 			});
-			return;
+		} else {
+			callback({error: "No school found"});
 		}
-		drivingSchoolCtrl.addStudentToSchool(schoolId, student);
-		callback(student);
+	},
+	(err) => {
+		callback(err);
 	});
 };
