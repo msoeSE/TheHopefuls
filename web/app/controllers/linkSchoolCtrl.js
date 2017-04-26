1fa2a7b85fc164872936dd71eb98fed868d66c13
Nate Dobbins <dobbinsn@msoe.edu>
2017-04-25 23:21:27 -0500

    making changes requested by dylan

--- web/app/controllers/linkSchoolCtrl.js
+++ web/app/controllers/linkSchoolCtrl.js
@@ -6,31 +6,31 @@ var drivingSchoolCtrl = require("../controllers/drivingSchoolCtrl");
 exports.linkAccToSchool = function(userId, schoolId, callback, error) {
 	drivingSchoolCtrl.getSchool(schoolId,
 	(results) => {
-		if(results != null){
-			User.findOneAndUpdate({
-				userId: userId
-			}, {
-				schoolId: schoolId
-			}, function(err, student) {
-				if (err) {
-					error({
-						"message": "Error finding student to link school with",
-						"error": err
-					});
-					return;
-				}
-				drivingSchoolCtrl.addStudentToSchool(schoolId, student,
-					(school) => {
-						callback({student: student, school: school});
-					}, (schoolError) => {
-						callback(schoolError);
-					});
-			});
-		} else {
-			callback({error: "No school found"});
+		if(results === undefined) {//eslint-disable-line
+			error({"message": "No School Found"});
+			return;
 		}
+		User.findOneAndUpdate({
+			userId: userId
+		}, {
+			schoolId: schoolId
+		}, function(err, student) {
+			if (err) {
+				error({
+					"message": "Error finding student to link school with",
+					"error": err
+				});
+				return;
+			}
+			drivingSchoolCtrl.addStudentToSchool(schoolId, student,
+				(school) => {
+					callback({student: student, school: school});
+				}, (schoolError) => {
+					error(schoolError);
+				});
+		});
 	},
 	(err) => {
-		callback(err);
+		error(err);
 	});
 };
