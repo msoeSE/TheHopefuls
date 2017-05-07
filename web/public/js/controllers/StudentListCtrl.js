25d8af9a6e205d9f9dee5d4589b4925ba146ea27
Nate Dobbins <dobbinsn@msoe.edu>
2017-05-07 14:41:34 -0500

    removed last log date

--- web/public/js/controllers/StudentListCtrl.js
+++ web/public/js/controllers/StudentListCtrl.js
@@ -11,12 +11,7 @@ angular.module("StudentListCtrl", ["StudentListService"])
 				render: function(data, type, full, meta){
 					return "<a href=\"/stats?id=" + full._id + "\">" +
 						full.firstName + " " + full.lastName + "</a>";
-				}},
-			{ title: "Last Log Date",
-				render: function(data, type, full, meta){
-					return "-";
-				},
-				defualtContent: "-"
+				}
 			},
 			{ title: "Action",
 				defaultContent: "<button class = 'waves-effect waves-light btn' type=\"button\">Remove</button>",
@@ -29,24 +24,4 @@ angular.module("StudentListCtrl", ["StudentListService"])
 		// TODO Remove from DB
 	});
 
-	// Mocked Data Stuff
-
-	vm.mockStudentData = [
-		["1","<a href=\"/stats?id=1\">Nathan Dobbins</a>", "1/08/2017"],
-    ["2","<a href=\"/stats?id=2\">Hardip Gill</a>", "1/07/2017"],
-    ["3","<a href=\"/stats?id=3\">Gerald Soriano</a>", "1/01/2017"],
-    ["4","<a href=\"/stats?id=4\">Dylan Walseth</a>", "1/12/2017"],
-    ["5","<a href=\"/stats?id=5\">Patrick Zawadzki</a>", "1/16/2017"]];
-
-	$(".test-table").DataTable({
-		data: vm.mockStudentData,
-		columns: [
-			{ title: "studentID", visible: false},
-			{ title: "Student Name" },
-			{ title: "Last Log Date" },
-			{ title: "Action",
-				defaultContent: "<button class = 'waves-effect waves-light btn' type=\"button\">Remove</button>",
-				width: "15%"}
-		]
-	});
 });
