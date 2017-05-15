06c6f7f0936a1c16aeeb8dc44fc65b7b4ce46bd0
Nate Dobbins <dobbinsn@msoe.edu>
2017-05-15 13:25:14 -0500

    fixed redirect on login bug

--- web/public/js/controllers/MainCtrl.js
+++ web/public/js/controllers/MainCtrl.js
@@ -1,7 +1,7 @@
 angular.module("MainCtrl", ["SettingsService"]).controller("MainController", function($location, $window, SettingsOb) {
 	var vm = this;
 
-	if($location.url() === "/"){
+	if($location.url() === "/" || $location.url() === "#_=_"){
 		SettingsOb.getProfile().then(function (response){
 			if(response.userType === "student"){
 				$window.location.href = "/stats";
